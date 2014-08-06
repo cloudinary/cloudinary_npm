1add1aa2ffa390cf72a4f4057c73cd64cec3be8e
Daniel Cohen <daniel@codeinvain.com>
2014-08-06 10:21:34 +0300

    fix: changed to public api cloudinary.url

--- samples/basic/basic.js
+++ samples/basic/basic.js
@@ -131,19 +131,19 @@ function performTransfromations(){
   console.log( ">> >> >> >> >> >> >> >> >> >>  Transformations << << << << << << << << << <<");
   console.log();
   console.log( "> Fit into 200x150");
-  console.log( "> " + cloudinary.utils.url(uploads.pizza2.public_id, {width: 200, height: 150, crop: "fit", format: "jpg"}));
+  console.log( "> " + cloudinary.url(uploads.pizza2.public_id, {width: 200, height: 150, crop: "fit", format: "jpg"}));
 
   console.log();
   console.log( "> Eager transformation of scaling to 200x150");
-  console.log( "> " + cloudinary.utils.url(uploads.lake.public_id, eager_options));
+  console.log( "> " + cloudinary.url(uploads.lake.public_id, eager_options));
 
   console.log();
   console.log( "> Face detection based 200x150 thumbnail");
-  console.log( "> " + cloudinary.utils.url(uploads.couple.public_id, {width: 200, height: 150, crop: "thumb", gravity: "faces", format: "jpg"}));
+  console.log( "> " + cloudinary.url(uploads.couple.public_id, {width: 200, height: 150, crop: "thumb", gravity: "faces", format: "jpg"}));
 
   console.log();
   console.log( "> Fill 200x150, round corners, apply the sepia effect");
-  console.log( "> " + cloudinary.utils.url(uploads.couple2.public_id, {width: 200, height: 150, crop: "fill", gravity: "face", radius: 10, effect:"sepia", format: "jpg"}));
+  console.log( "> " + cloudinary.url(uploads.couple2.public_id, {width: 200, height: 150, crop: "fill", gravity: "face", radius: 10, effect:"sepia", format: "jpg"}));
 
   console.log();
   console.log( "> That's it. You can now open the URLs above in a browser");
