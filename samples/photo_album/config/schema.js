b43681cb3cda42b2157da10b2555ff5b366e241a
Daniel Cohen <daniel@codeinvain.com>
2014-08-19 12:17:55 +0300

    using Cloudinary gem to generate images and urls

--- samples/photo_album/config/schema.js
+++ samples/photo_album/config/schema.js
@@ -1,26 +1,12 @@
 var Schema = require('promised-jugglingdb').Schema;
-var cloudinary = require('cloudinary').v2;
 var schema = new Schema('memory');
 // uncomment if you want to use mongodb adapter
-//var schema = new Schema('mongodb');
+var schema = new Schema('mongodb');
 
 // define models
 var Photo = schema.define('Photo', {
   title      : { type : String, length   : 255 },
   image      : { type : JSON}
 });
-
-Photo.prototype.thumbnailUrl = function(){
-  if(this.image){
-    return cloudinary.url(this.image.public_id, {width: 150, height: 150, quality: 80}); 
-  }else{
-    return "";
-  }
-}
-
-Photo.prototype.transformationUrl = function(transformation){
-  if (transformation==null) transformation = {};
-  return cloudinary.url(this.image.public_id, transformation); 
-}
 
 module.exports = schema;
