88c8caaa885a301ac24bf6fcbe57a30f11583687
Shahar Soel <shahars@protonmail.com>
2019-05-15 21:42:01 +0300

    Removed legacy versions of nodejs. Node 4 and 6 have reached End Of Life: - https://github.com/nodejs/Release#end-of-life-releases
    This means that the compilation using Babel to ES5 is no longer
    needed which reduces the complexity of this project.
    
    Note that nodejs version 12 was added to the travis test suite.

--- cloudinary.js
+++ cloudinary.js
@@ -1,1 +1,1 @@
-module.exports = Number(process.versions.node.split('.')[0]) < 8 ? require('./lib-es5/cloudinary') : require('./lib/cloudinary');
+module.exports = require('./lib/cloudinary');
--- lib-es5/v2/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var v1 = require('../cloudinary.js');
-var clone = require('lodash/clone');
-var v2 = clone(v1);
-v2.api = require('./api');
-v2.uploader = require('./uploader');
-v2.search = require('./search');
-module.exports = v2;
