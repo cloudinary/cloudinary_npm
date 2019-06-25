f2c84870664ba60a47da48bdb44ebb5d40294583
Raya Straus <raya@cloudinary.com>
2019-06-25 11:47:29 +0300

    Add antialiasing and hinting

--- lib-es5/uploader.js
+++ lib-es5/uploader.js
@@ -241,7 +241,7 @@ exports.rename = function rename(from_public_id, to_public_id, callback) {
   });
 };
 
-var TEXT_PARAMS = ["public_id", "font_family", "font_size", "font_color", "text_align", "font_weight", "font_style", "background", "opacity", "text_decoration"];
+var TEXT_PARAMS = ["public_id", "font_family", "font_size", "font_color", "text_align", "font_weight", "font_style", "background", "opacity", "text_decoration", "font_hinting", "font_antialiasing"];
 
 exports.text = function text(content, callback) {
   var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
