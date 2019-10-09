a5334a60d89f3bc407c6fcdce08ed14894cb3012
Brian Luk <brian@cloudinary.com>
2019-10-09 14:16:21 -0700

    Add test for upload function to allow signature parameter

--- test/utils_spec.js
+++ test/utils_spec.js
@@ -1425,45 +1425,45 @@ describe("utils", function () {
         api_key: "1234",
       });
     });
-    it("should allow timestamp in sign_request", function() {
+    it("should allow timestamp in sign_request", function () {
       var params = utils.sign_request({
         public_id: "folder/file",
         version: "1234",
-        timestamp: 1569707219
+        timestamp: 1569707219,
       });
-      expect(params).to.eql({ 
+      expect(params).to.eql({
         public_id: 'folder/file',
         version: '1234',
         timestamp: 1569707219,
         signature: 'b77fc0b0dffbf7e74bdad36b615225fb6daff81e',
-        api_key: '1234' 
+        api_key: '1234',
       });
     });
-    it("should allow a signature parameter in process_request_params", function() {
-      var configBck = void 0;
+    it("should allow a signature parameter in process_request_params", function () {
+      var configBck2 = void 0;
       before(function () {
-        configBck = cloudinary.config();
+        configBck2 = cloudinary.config();
         cloudinary.config({
           api_key: "1234",
           api_secret: "",
         });
       });
       after(function () {
-        cloudinary.config(configBck);
+        cloudinary.config(configBck2);
       });
-      var params = utils.process_request_params({}, { 
+      var params = utils.process_request_params({}, {
         public_id: 'folder/file',
         version: '1234',
         timestamp: 1569707219,
         signature: 'b77fc0b0dffbf7e74bdad36b615225fb6daff81e',
-        api_key: '1234' 
+        api_key: '1234',
       });
-      expect(params).to.eql({ 
+      expect(params).to.eql({
         public_id: 'folder/file',
         version: '1234',
         timestamp: 1569707219,
         signature: 'b77fc0b0dffbf7e74bdad36b615225fb6daff81e',
-        api_key: '1234' 
+        api_key: '1234',
       });
     });
   });
