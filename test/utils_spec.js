f98fe0718e083a650ee130579d21e44ba52615b6
RTLcoil <RTLcoil@users.noreply.github.com>
2020-04-27 16:59:36 +0300

    Fix normalize_expression when variable is named like a keyword (e.g., "$width") (#367)
    * Fix `normalize_expression` to ignore predefined variables

--- test/utils_spec.js
+++ test/utils_spec.js
@@ -1052,6 +1052,21 @@ describe("utils", function () {
         t = cloudinary.utils.generate_transformation_string(options);
         expect(t).to.eql("$foo_10/if_fc_gt_2/c_scale,w_$foo_mul_200_div_fc/if_end");
       });
+      it("should not change variable names even if they look like keywords", function () {
+        var options, t;
+        options = {
+          transformation: [
+            {
+              $width: 10,
+            },
+            {
+              width: "$width + 10 + width",
+            },
+          ],
+        };
+        t = cloudinary.utils.generate_transformation_string(options);
+        expect(t).to.eql("$width_10/w_$width_add_10_add_w");
+      });
       it("should support text values", function () {
         test_cloudinary_url("sample", {
           effect: "$efname:100",
