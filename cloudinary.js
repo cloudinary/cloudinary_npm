e762eeeab1e6d4c199b839a4f9c35d4f8ec87609
patrick-tolosa <59408474+patrick-tolosa@users.noreply.github.com>
2020-05-21 10:32:04 +0300

    Add deprecation warning for node 6, add tests for node 14 (#389)

--- cloudinary.js
+++ cloudinary.js
@@ -1,1 +1,6 @@
-module.exports = Number(process.versions.node.split('.')[0]) < 8 ? require('./lib-es5/cloudinary') : require('./lib/cloudinary');
+if (Number(process.versions.node.split('.')[0]) < 8) {
+  console.warn('DEPRECATION NOTICE - Node 6 has been scheduled for removal from the Cloudinary SDK')
+  module.exports = require('./lib-es5/cloudinary');
+} else {
+  module.exports = require('./lib/cloudinary');
+}
