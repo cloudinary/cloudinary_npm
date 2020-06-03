e4ad379cdce5267ea3aa1e5445af0348b113e008
Patrick Tolosa <patrick.tolosa@cloudinary.com>
2020-06-03 16:35:31 +0300

    Set default to false (instead of true), and added a test to enforce it

--- test/utils/image_versionID_spec.js
+++ test/utils/image_versionID_spec.js
@@ -27,6 +27,19 @@ describe('Tests for sdk versionID through image tag', function () {
     process.versions = processVersions;
   });
 
+  it('Defaults to false if analytics is not passed as an option', () => {
+    process.versions = {
+      node: '12.0.0',
+    };
+
+    let imgStr = cloudinary.image("hello", {
+      format: "png",
+    });
+
+    expect(getSDKVersionID()).to.equal('MAlhAM0'); // value is mocked through package.json and process.version
+    expect(imgStr).not.to.contain(`MAlhAM0`);
+  });
+
   it('Reads from process.versions and package.json (Mocked)', () => {
     process.versions = {
       node: '12.0.0',
@@ -34,6 +47,7 @@ describe('Tests for sdk versionID through image tag', function () {
 
     let imgStr = cloudinary.image("hello", {
       format: "png",
+      analytics: true
     });
 
     expect(getSDKVersionID()).to.equal('MAlhAM0'); // value is mocked through package.json and process.version
@@ -48,9 +62,9 @@ describe('Tests for sdk versionID through image tag', function () {
     let imgStr = cloudinary.image("hello", {
       format: "png",
       responsive: true,
+      analytics: true
     });
 
-    console.log(process.env);
     expect(getSDKVersionID()).to.equal('MAlhAM0'); // value is mocked through package.json and process.version
     expect(imgStr).to.contain(`src='http://res.cloudinary.com/${TEST_CLOUD_NAME}/image/upload/hello.png?_s=MAlhAMA`);
   });
