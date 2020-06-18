2405a68ce9ef05b0afcbf52dae779e9df6a48f1d
patrick <patrick.tolosa@gmail.com>
2020-06-18 19:35:21 +0300

    Test commit

--- cloudinary.js
+++ cloudinary.js
@@ -1,3 +1,4 @@
+// Ignore this
 if (Number(process.versions.node.split('.')[0]) < 8) {
   console.warn('DEPRECATION NOTICE - Node 6 has been scheduled for removal from the Cloudinary SDK');
   // required polyfills for native ES6/7 functions (such as String.prototype.padStart)
