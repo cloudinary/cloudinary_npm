3b9b9970cffb7c0160db38f2281f0036aeb38643
patrick-tolosa <59408474+patrick-tolosa@users.noreply.github.com>
2020-07-20 17:39:50 +0300

    Prepare using setup.js as the main entrypoint for tests (#426)
    * Prepare using setup.js as the main entrypoint for tests

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,10 @@
-global.expect = require('expect.js');
-require('./testUtils/testBootstrap');
-
-
 require('dotenv').load({
   silent: true
 });
 
+if (!process.env.CLOUDINARY_URL) {
+  throw 'Could not start tests - Cloudianry URL is undefined'
+}
 
+global.expect = require('expect.js');
+require('./testUtils/testBootstrap');
