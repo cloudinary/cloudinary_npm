3f7e3c129a7e93289e8b2e19c4cec179f109c371
Patrick Tolosa <patrick.tolosa@cloudinary.com>
2020-07-20 16:36:01 +0300

    Add support for Node 6 and 8

--- tools/scripts/test.es5.sh
+++ tools/scripts/test.es5.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-mocha --require 'babel-register' --require 'babel-polyfill' "./test/**/*spec.js"
+# Mocha 8 does not support node < 10
+# Force Mocha 6, can be removed once we drop support for Node 6
+npm install mocha@6
+mocha --require './test/setup.js' --require 'babel-register' --require 'babel-polyfill' "./test/**/*spec.js"
--- tools/scripts/test.sh
+++ tools/scripts/test.sh
@@ -3,9 +3,15 @@ set -e;
 
 node_v=$(node --version) ;
 npm run lint
-if [[ "${node_v%%.*}" == 'v4' || "${node_v%%.*}" == 'v6' ]]
-then
+
+if [[ "${node_v%%.*}" == 'v4' || "${node_v%%.*}" == 'v6' ]]; then
   npm run test-es5
+
+elif [[ "${node_v%%.*}" == 'v8' || "${node_v%%.*}" == 'v8' ]]; then
+  # Mocha 8 does not support Node < 10,
+  # Force Mocha 6, can be removed once we drop support for Node 8
+  npm install mocha@6
+  npm run test-es6
 else
   npm run test-es6
 fi
