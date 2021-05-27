f39b46c356d4742263d5dbb2db14c360ebadfa39
Raya Straus <raya@cloudinary.com>
2021-05-27 19:26:54 +0300

    update test

--- test/integration/api/authorization/oAuth_authorization_spec.js
+++ test/integration/api/authorization/oAuth_authorization_spec.js
@@ -56,17 +56,6 @@ describe("oauth_token", function(){
       ).catch(({ error }) => expect(error.message).to.contain("unknown api_key"));
   });
 
-  it("Test oauth_token as only option (admin_api)", function() {
-    cloudinary.config({
-      api_key: null,
-      api_secret: null
-    });
-    return cloudinary.v2.api.resource(PUBLIC_ID, {oauth_token: 'MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI4'})
-      .then(
-        () => expect().fail()
-      ).catch(({ error }) => expect(error.message).to.contain("Invalid token"));
-  });
-
   it("oauth_token as option should take priority with secret and key (admin_api)", function() {
     cloudinary.config({
       api_key: '1234',
@@ -77,29 +66,6 @@ describe("oauth_token", function(){
         () => expect().fail()
       ).catch(({ error }) => expect(error.message).to.contain("Invalid token"));
   });
-
-  it("oauth_token as config should take priority with secret and key (admin_api)", function() {
-    cloudinary.config({
-      api_key: '123',
-      api_secret: '123',
-      oauth_token: 'MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI4'
-    });
-    return cloudinary.v2.api.resource(PUBLIC_ID)
-      .then(
-        () => expect().fail()
-      ).catch(({ error }) => expect(error.message).to.contain("Invalid token"));
-  });
-
-
-
-
-
-
-
-
-
-
-
 
   it("should send the oauth_token option to the server (upload_api)", function() {
     return helper.provideMockObjects((mockXHR, writeSpy, requestSpy) => {
@@ -134,7 +100,7 @@ describe("oauth_token", function(){
     });
     return helper.provideMockObjects((mockXHR, writeSpy, requestSpy) => {
       cloudinary.v2.uploader.upload(PUBLIC_ID)
-      return sinon.assert.calledWith(requestSpy, sinon.match({ auth: "1234:1234" }));
+      return sinon.assert.calledWith(requestSpy, sinon.match({ auth: null }));
     });
   });
 
@@ -144,43 +110,9 @@ describe("oauth_token", function(){
       api_secret: null,
       oauth_token: null
     });
-    cloudinary.v2.uploader.upload(PUBLIC_ID)
-      .then(
-        () => expect().fail()
-      ).catch(({ error }) => expect(error.message).to.contain("unknown api_key"));
-  });
-
-  it("Test oauth_token as only option (upload_api)", function() {
-    cloudinary.config({
-      api_key: null,
-      api_secret: null
+    return helper.provideMockObjects((mockXHR, writeSpy, requestSpy) => {
+      cloudinary.v2.uploader.upload(PUBLIC_ID)
+      return sinon.assert.calledWith(requestSpy, sinon.match({ auth: null }));
     });
-    return cloudinary.v2.uploader.upload(PUBLIC_ID, {oauth_token: 'MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI4'})
-      .then(
-        () => expect().fail()
-      ).catch(({ error }) => expect(error.message).to.contain("Invalid token"));
-  });
-
-  it("oauth_token as option should take priority with secret and key (upload_api)", function() {
-    cloudinary.config({
-      api_key: '1234',
-      api_secret: '1234'
-    });
-    return cloudinary.v2.uploader.upload(PUBLIC_ID, {oauth_token: 'MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI4'})
-      .then(
-        () => expect().fail()
-      ).catch(({ error }) => expect(error.message).to.contain("Invalid token"));
-  });
-
-  it("oauth_token as config should take priority with secret and key (upload_api)", function() {
-    cloudinary.config({
-      api_key: '123',
-      api_secret: '123',
-      oauth_token: 'MTQ0NjJkZmQ5OTM2NDE1ZTZjNGZmZjI4'
-    });
-    return cloudinary.v2.api.resource(PUBLIC_ID)
-      .then(
-        () => expect().fail()
-      ).catch(({ error }) => expect(error.message).to.contain("Invalid token"));
   });
 });
