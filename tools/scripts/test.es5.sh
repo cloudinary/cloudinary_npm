c70d515224b1e612e31a45a22e0ae3b7dfdc6723
strausr <git config --global user.email>
2022-08-22 17:52:42 +0300

    update core-js dependency for es5 tests

--- tools/scripts/test.es5.sh
+++ tools/scripts/test.es5.sh
@@ -4,4 +4,5 @@
 # This file should be in the config under a 'require' key
 # However Mocha 6 does not expose before, beforeEach after etc. at that time
 # When Removing support of Node 6 and 8 and using Mocha 8, we should move this to the mocharc.json file
+npm install core-js@3.6.5
 mocha --require './test/setup.js' --require 'babel-register' --require 'babel-polyfill' "./test/**/*spec.js"
