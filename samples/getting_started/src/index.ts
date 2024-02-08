11c5d45bb0d5de003ce0a699cd2a3b2aa3877f4f
strausr <git config --global user.email>
2024-02-08 09:34:00 +0200

    add getting started sample

---
+++ samples/getting_started/src/index.ts
@@ -0,0 +1,48 @@
+import {v2 as cloudinary} from 'cloudinary';
+
+// Update to your credentials
+cloudinary.config({
+    cloud_name: '<cloud_name>',
+    api_key: '<api_key>',
+    api_secret: '<api_secret>'
+});
+
+
+// register to auto tagging https://cloudinary.com/documentation/google_auto_tagging_addon
+// register to background removal https://cloudinary.com/documentation/cloudinary_ai_background_removal_addon
+//Upload an image with auto tag
+cloudinary.uploader.upload(
+    "https://res.cloudinary.com/demo/image/upload/v1707306308/cld-docs-hp/walking_woman",
+    { public_id: "walking_woman", categorization: "google_tagging" })
+    .then((result)=>{
+        console.log(result.info.categorization.google_tagging)}).catch((error)=> {console.log(error)});
+
+// log in to you cloudinary account. Navigate to the newly uploaded `walking_woman` image. Select open on the image
+// and navigate to the Metadata tab. Select Auto tags, select plus on the Overcoat tag to tag your image.
+
+//Upload an image for you underlay
+cloudinary.uploader.upload(
+    "https://res.cloudinary.com/demo/image/upload/v17073124415555/cld-docs-hp/street.jpg",
+    { public_id: "street" })
+    .then((result)=>{
+        console.log(result)}).catch((error)=> {console.log(error)});
+
+
+//Transformation image
+const url = cloudinary.url("walking_woman", {
+    transformation: [
+        { effect: "background_removal" },
+        { gravity: "auto", crop: "auto", width: 450 },
+        { underlay: "street" },
+        { format: "auto", quality: "auto"},
+        {if: "!Overcoat!_in_tags"},
+        {
+            overlay:
+                { font_family: "Arial", font_size: 25,  letter_spacing: 14, font_weight:"bold", text: "NEW COLLECTION" },
+            color: "white"
+        },
+        {if: "end"},
+    ]
+});
+
+console.log("this is the transformed url", url);
--- samples/readme.md
+++ samples/readme.md
@@ -1,5 +1,17 @@
 # Cloudinary Node Sample Projects #
 
+## Getting started sample
+
+The getting started sample uploads remote image to Cloudinary and generates URLs for applying various image transformations using addons on the uploaded files.
+
+### Setting up
+1. Before running the sample, ensure you copy your credentials and replace them in `index.ts`. You can find your credentials on the Cloudinary Getting Started section.
+2. Ensure you register to the Auto Tagging addon https://cloudinary.com/documentation/google_auto_tagging_addon
+3. Ensure you register to the Background Removal addon https://cloudinary.com/documentation/cloudinary_ai_background_removal_addon
+4. In the Getting Started sample directory, run `npm install` in project directory to bring all the required modules.
+5. Run the sample using `npm start`
+
+
 ## Basic sample
 
 The basic sample uploads local and remote image to Cloudinary and generates URLs for applying various image transformations on the uploaded files.
