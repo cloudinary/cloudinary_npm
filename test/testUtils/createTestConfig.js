cb6acab5781325e09a00da1fda5597d23786e426
Patryk Konior <97894263+cloudinary-pkoniu@users.noreply.github.com>
2024-07-16 17:01:49 +0200

    Fix/url analytics property name (#670)
    * fix: add support snake case casing of analytics options

--- test/testUtils/createTestConfig.js
+++ test/testUtils/createTestConfig.js
@@ -1,12 +1,13 @@
 const defaultConfigOptions = {
-  urlAnalytics: false
+  urlAnalytics: false,
+  analytics: false
 };
 
 /**
  * @description Creates a default config for testing, add properties to defaultConfigOptions to
  *              make them global across all tests
  * @param {{}} [confOptions] Cloudinary's config options
- * @return {{} & {urlAnalytics: true} & any}
+ * @return {{} & {analytics: true} & any}
  */
 function createTestConfig(confOptions = {}) {
   return Object.assign({}, defaultConfigOptions, confOptions);
