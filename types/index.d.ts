4bb6b66788628c154b6f24f2a3df43e208db37df
Matei Trandafir <catalin_trand2@yahoo.com>
2024-09-25 00:46:16 +0300

    fix: metadata field datasource type

--- types/index.d.ts
+++ types/index.d.ts
@@ -710,7 +710,9 @@ declare module 'cloudinary' {
         mandatory?: boolean;
         default_value?: number;
         validation?: object; //there are 4 types, we need to discuss documentation team about it before implementing.
-        datasource?: DatasourceEntry;
+        datasource?: {
+            values: Array<DatasourceEntry>
+        };
         default_disabled?: boolean;
 
         [futureKey: string]: any;
