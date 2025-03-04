d6c51e2f5dfb66be92b123d353e9be067e3b960e
Danny Valentine <81298353+dannyv-cloudinary@users.noreply.github.com>
2025-03-04 18:10:27 +0000

    feat: Add support for DELETE /resources/backup/:asset_id (#700)
    * feat: Add support for DELETE /resources/backup/:asset_id
    
    * Remove testing bloat
    
    ---------
    
    Co-authored-by: cloudinary-pkoniu <patryk.konior@cloudinary.com>

--- test/integration/api/admin/api_spec.js
+++ test/integration/api/admin/api_spec.js
@@ -921,6 +921,60 @@ describe("api", function () {
       });
     });
   });
+  describe('delete_backed_up_assets', function () {
+    it('should delete specific version IDs', async function () {
+      this.timeout(TIMEOUT.LARGE);
+
+      // Process:
+      // - Upload to the same public ID three times
+      // - Delete a single version (string)
+      // - Delete a two versions (array)
+      // - Cleanup
+
+      // Perform three uploads
+      const firstUpload = await uploadImage({
+        public_id: PUBLIC_ID_BACKUP_1,
+        backup: true
+      });
+
+      const secondUpload = await uploadImage({
+        public_id: PUBLIC_ID_BACKUP_1,
+        backup: true,
+        angle: '0', // To create a unique version
+        overwrite: true
+      });
+
+      const thirdUpload = await uploadImage({
+        public_id: PUBLIC_ID_BACKUP_1,
+        backup: true,
+        angle: '100', // To create a unique version
+        overwrite: true
+      });
+
+      // Ensure all files were uploaded correctly
+      expect(firstUpload).not.to.be(null);
+      expect(secondUpload).not.to.be(null);
+      expect(thirdUpload).not.to.be(null);
+
+      // Get the asset ID and versions of the uploaded asset
+      const resourceResp = await API_V2.resource(PUBLIC_ID_BACKUP_1, {versions: true});
+      const assetId = resourceResp.asset_id;
+      const firstAssetVersion = resourceResp.versions[0].version_id;
+      const secondAssetVersion = resourceResp.versions[1].version_id;
+      const thirdAssetVersion = resourceResp.versions[2].version_id;
+
+      // Delete the first version
+      const removeSingleVersion = await cloudinary.v2.api.delete_backed_up_assets(assetId, firstAssetVersion);
+      const removeSingleVersionResp = await API_V2.resource(PUBLIC_ID_BACKUP_1, {versions: true});
+      expect(removeSingleVersionResp.versions).not.to.contain(firstAssetVersion);
+
+      // Delete the remaining two versions
+      const removeMultipleVersions = await cloudinary.v2.api.delete_backed_up_assets(assetId, [secondAssetVersion, thirdAssetVersion]);
+      const removeMultipleVersionsResp = await API_V2.resource(PUBLIC_ID_BACKUP_1, {versions: true});
+      expect(removeMultipleVersionsResp.versions).not.to.contain(secondAssetVersion);
+      expect(removeMultipleVersionsResp.versions).not.to.contain(thirdAssetVersion);
+    });
+  });
   describe("update", function () {
     describe("notification url", function () {
       var writeSpy, xhr;
